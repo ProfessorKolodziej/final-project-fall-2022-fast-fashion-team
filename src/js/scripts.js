2ae6686f4b4b87b1c2606e75e34ce56e155959fe
aihuaboothroyd <abooth05@bu.edu>
2022-11-16 18:49:17 -0500

    update javascript

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,6 @@
 const welcome = prompt('Type yes to proceed to our website');
-if (welcome === 'yes') {
-	alert('Welcome to our website');
-} else if (welcome !== 'yes') {
-	alert("sorry you can't view this website");
-}
+ if (welcome === 'yes') {
+ 	alert('Welcome to our website');
+ } else if (welcome !== 'yes') {
+ 	alert("Sorry, try again");
+ }
